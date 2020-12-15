4136e15adf91d2d799ce8dd434581575d69c3947
sandesh85210@gmail.com <sandesh85210@gmail.com>
2020-12-15 12:16:58 +0530

    firebase init

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// imports 
+const express = require('express');
+const app = express()
+const functions = require('firebase-functions')
+const server = require('http').Server(app);
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+// listen
+app.get('/', (req, res)=>{
+    return res.status(200).json("success");
+})
+exports.api = functions.https.onRequest(app);
